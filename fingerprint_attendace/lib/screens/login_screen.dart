fb5d59a99a973a39aba715d08f8afda83cca3276
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2024-07-22 15:37:22 +0300

    Login screen CSS

--- fingerprint_attendace/lib/screens/login_screen.dart
+++ fingerprint_attendace/lib/screens/login_screen.dart
@@ -17,62 +17,101 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                controller: _nameController,
-                decoration: InputDecoration(labelText: 'Name'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your name';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    bool authenticated = await _employeeDao.authenticateUser(
-                      _nameController.text.toLowerCase(), // Convert name to lowercase
-                      _passwordController.text,
-                    );
-                    if (authenticated) {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => HomeScreen(username: _nameController.text),
-                        ),
-                      );
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('Invalid name or password')),
-                      );
-                    }
-                  }
-                },
-                child: Text('Login'),
-              ),
-            ],
-          ),
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/company_logo.jpg',
+              height: 40,
+              width: 40,
+            ),
+            SizedBox(width: 10),
+            Text(
+              'Login',
+              style: TextStyle(color: Colors.white), // AppBar title color to white
+            ),
+          ],
         ),
+        backgroundColor: Color(0xFF930000),
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+      ),
+      body: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextFormField(
+                      controller: _nameController,
+                      decoration: InputDecoration(labelText: 'Name'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your name';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(labelText: 'Password'),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            bool authenticated = await _employeeDao.authenticateUser(
+                              _nameController.text.toLowerCase(),
+                              _passwordController.text,
+                            );
+                            if (authenticated) {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => HomeScreen(username: _nameController.text),
+                                ),
+                              );
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text('Invalid name or password')),
+                              );
+                            }
+                          }
+                        },
+                        child: Text('Login'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/logo.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
