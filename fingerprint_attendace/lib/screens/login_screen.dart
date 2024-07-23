765fe5d89f6b149d578f2edf3a3e8adc9cc4998c
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2024-07-23 12:05:57 +0300

    Update login_screen.dart

--- fingerprint_attendace/lib/screens/login_screen.dart
+++ fingerprint_attendace/lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 import '../database/dao/employee_dao.dart';
+import 'dart:math';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -11,7 +12,11 @@ class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _codeController = TextEditingController();
   final EmployeeDao _employeeDao = EmployeeDao();
+
+  bool _isCodeSent = false;
+  String? _verificationCode;
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +32,7 @@ class _LoginScreenState extends State<LoginScreen> {
             SizedBox(width: 10),
             Text(
               'Login',
-              style: TextStyle(color: Colors.white), // AppBar title color to white
+              style: TextStyle(color: Colors.white),
             ),
           ],
         ),
@@ -54,54 +59,87 @@ class _LoginScreenState extends State<LoginScreen> {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: <Widget>[
-                              TextFormField(
-                                controller: _emailController,
-                                decoration: InputDecoration(labelText: 'Email'),
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Please enter your email';
-                                  }
-                                  return null;
-                                },
-                              ),
-                              SizedBox(height: 16),
-                              TextFormField(
-                                controller: _passwordController,
-                                decoration: InputDecoration(labelText: 'Password'),
-                                obscureText: true,
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Please enter your password';
-                                  }
-                                  return null;
-                                },
-                              ),
-                              SizedBox(height: 20),
-                              Center(
-                                child: ElevatedButton(
-                                  onPressed: () async {
-                                    if (_formKey.currentState!.validate()) {
-                                      bool authenticated = await _employeeDao.authenticateUser(
-                                        _emailController.text.toLowerCase(),
-                                        _passwordController.text,
-                                      );
-                                      if (authenticated) {
+                              if (!_isCodeSent) ...[
+                                TextFormField(
+                                  controller: _emailController,
+                                  decoration: InputDecoration(labelText: 'Email'),
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter your email';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                SizedBox(height: 16),
+                                TextFormField(
+                                  controller: _passwordController,
+                                  decoration: InputDecoration(labelText: 'Password'),
+                                  obscureText: true,
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter your password';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                SizedBox(height: 20),
+                                Center(
+                                  child: ElevatedButton(
+                                    onPressed: () async {
+                                      if (_formKey.currentState!.validate()) {
+                                        bool authenticated = await _employeeDao.authenticateUser(
+                                          _emailController.text.toLowerCase(),
+                                          _passwordController.text,
+                                        );
+                                        if (authenticated) {
+                                          // Generate and send verification code
+                                          _verificationCode = _generateVerificationCode();
+                                          await _sendVerificationCode(_emailController.text, _verificationCode!);
+
+                                          setState(() {
+                                            _isCodeSent = true;
+                                          });
+                                        } else {
+                                          ScaffoldMessenger.of(context).showSnackBar(
+                                            SnackBar(content: Text('Invalid email or password')),
+                                          );
+                                        }
+                                      }
+                                    },
+                                    child: Text('Login'),
+                                  ),
+                                ),
+                              ] else ...[
+                                TextFormField(
+                                  controller: _codeController,
+                                  decoration: InputDecoration(labelText: 'Verification Code'),
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter the verification code';
+                                    }
+                                    if (value != _verificationCode) {
+                                      return 'Invalid verification code';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                SizedBox(height: 20),
+                                Center(
+                                  child: ElevatedButton(
+                                    onPressed: () async {
+                                      if (_formKey.currentState!.validate()) {
                                         Navigator.pushReplacement(
                                           context,
                                           MaterialPageRoute(
                                             builder: (context) => HomeScreen(email: _emailController.text),
                                           ),
                                         );
-                                      } else {
-                                        ScaffoldMessenger.of(context).showSnackBar(
-                                          SnackBar(content: Text('Invalid email or password')),
-                                        );
                                       }
-                                    }
-                                  },
-                                  child: Text('Login'),
+                                    },
+                                    child: Text('Verify and Login'),
+                                  ),
                                 ),
-                              ),
+                              ],
                             ],
                           ),
                         ),
@@ -113,7 +151,7 @@ class _LoginScreenState extends State<LoginScreen> {
                         decoration: BoxDecoration(
                           image: DecorationImage(
                             image: AssetImage('assets/logo.png'),
-                            fit: BoxFit.contain, // Ensure the entire image is visible
+                            fit: BoxFit.contain,
                           ),
                         ),
                       ),
@@ -123,12 +161,12 @@ class _LoginScreenState extends State<LoginScreen> {
               : Column(
                   children: [
                     Container(
-                      width: double.infinity, // Make sure it covers the width
-                      height: 200, // Adjust height as needed
+                      width: double.infinity,
+                      height: 200,
                       decoration: BoxDecoration(
                         image: DecorationImage(
                           image: AssetImage('assets/logo.png'),
-                          fit: BoxFit.contain, // Ensure the entire image is visible
+                          fit: BoxFit.contain,
                         ),
                       ),
                     ),
@@ -141,54 +179,86 @@ class _LoginScreenState extends State<LoginScreen> {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: <Widget>[
-                              TextFormField(
-                                controller: _emailController,
-                                decoration: InputDecoration(labelText: 'Email'),
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Please enter your email';
-                                  }
-                                  return null;
-                                },
-                              ),
-                              SizedBox(height: 16),
-                              TextFormField(
-                                controller: _passwordController,
-                                decoration: InputDecoration(labelText: 'Password'),
-                                obscureText: true,
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Please enter your password';
-                                  }
-                                  return null;
-                                },
-                              ),
-                              SizedBox(height: 20),
-                              Center(
-                                child: ElevatedButton(
-                                  onPressed: () async {
-                                    if (_formKey.currentState!.validate()) {
-                                      bool authenticated = await _employeeDao.authenticateUser(
-                                        _emailController.text.toLowerCase(),
-                                        _passwordController.text,
-                                      );
-                                      if (authenticated) {
+                              if (!_isCodeSent) ...[
+                                TextFormField(
+                                  controller: _emailController,
+                                  decoration: InputDecoration(labelText: 'Email'),
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter your email';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                SizedBox(height: 16),
+                                TextFormField(
+                                  controller: _passwordController,
+                                  decoration: InputDecoration(labelText: 'Password'),
+                                  obscureText: true,
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter your password';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                SizedBox(height: 20),
+                                Center(
+                                  child: ElevatedButton(
+                                    onPressed: () async {
+                                      if (_formKey.currentState!.validate()) {
+                                        bool authenticated = await _employeeDao.authenticateUser(
+                                          _emailController.text.toLowerCase(),
+                                          _passwordController.text,
+                                        );
+                                        if (authenticated) {
+                                          _verificationCode = _generateVerificationCode();
+                                          await _sendVerificationCode(_emailController.text, _verificationCode!);
+
+                                          setState(() {
+                                            _isCodeSent = true;
+                                          });
+                                        } else {
+                                          ScaffoldMessenger.of(context).showSnackBar(
+                                            SnackBar(content: Text('Invalid email or password')),
+                                          );
+                                        }
+                                      }
+                                    },
+                                    child: Text('Login'),
+                                  ),
+                                ),
+                              ] else ...[
+                                TextFormField(
+                                  controller: _codeController,
+                                  decoration: InputDecoration(labelText: 'Verification Code'),
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter the verification code';
+                                    }
+                                    if (value != _verificationCode) {
+                                      return 'Invalid verification code';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                SizedBox(height: 20),
+                                Center(
+                                  child: ElevatedButton(
+                                    onPressed: () async {
+                                      if (_formKey.currentState!.validate()) {
                                         Navigator.pushReplacement(
                                           context,
                                           MaterialPageRoute(
                                             builder: (context) => HomeScreen(email: _emailController.text),
                                           ),
                                         );
-                                      } else {
-                                        ScaffoldMessenger.of(context).showSnackBar(
-                                          SnackBar(content: Text('Invalid email or password')),
-                                        );
                                       }
-                                    }
-                                  },
-                                  child: Text('Login'),
+                                    },
+                                    child: Text('Verify and Login'),
+                                  ),
                                 ),
-                              ),
+                              ],
                             ],
                           ),
                         ),
@@ -199,5 +269,16 @@ class _LoginScreenState extends State<LoginScreen> {
         },
       ),
     );
+  }
+
+  String _generateVerificationCode() {
+    final random = Random();
+    final code = random.nextInt(900000) + 100000;
+    return code.toString();
+  }
+
+  Future<void> _sendVerificationCode(String email, String code) async {
+    // Implement your email sending logic here
+    print('Sending verification code $code to $email');
   }
 }
